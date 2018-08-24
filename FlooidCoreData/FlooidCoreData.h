6c982423fa7998e060cbd8ab5d862d541098c9a3
Your Name <you@example.com>
2018-08-24 07:56:29 +0300

    Initial Commit

---
+++ FlooidCoreData/FlooidCoreData.h
@@ -0,0 +1,19 @@
+//
+//  FlooidCoreData.h
+//  FlooidCoreData
+//
+//  Created by Martin Lalev on 24.08.18.
+//  Copyright © 2018 Martin Lalev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlooidCoreData.
+FOUNDATION_EXPORT double FlooidCoreDataVersionNumber;
+
+//! Project version string for FlooidCoreData.
+FOUNDATION_EXPORT const unsigned char FlooidCoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlooidCoreData/PublicHeader.h>
+
+
