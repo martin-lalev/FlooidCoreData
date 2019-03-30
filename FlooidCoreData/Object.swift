473d4bdce78f822f7a62ed59bff238f6cc19cebd
Your Name <you@example.com>
2019-03-30 10:55:05 +0200

    Migrated to swift 5 and xcode 10.2

--- FlooidCoreData/Object.swift
+++ FlooidCoreData/Object.swift
@@ -16,32 +16,32 @@ public protocol DataObjectProtocol: class {
 
 public extension DataObjectProtocol where Self: NSManagedObject {
     
-    public func threadSafe() -> ThreadSafeCoreDataObject<Self> {
+    func threadSafe() -> ThreadSafeCoreDataObject<Self> {
         return ThreadSafeCoreDataObject(self.objectID)
     }
     
-    public static func query(in context: CoreDataContext) -> CoreDataQuery<Self> {
+    static func query(in context: CoreDataContext) -> CoreDataQuery<Self> {
         return CoreDataQuery(for: context)
     }
     
-    public static func object(forID id: String, in context:CoreDataContext) -> Self? {
+    static func object(forID id: String, in context:CoreDataContext) -> Self? {
         return Self.query(in: context).filter(NSPredicate(format: "\( self.idKey() ) = %@", id)).execute().first
     }
     
-    public static func create(forID id: String, in context:CoreDataContext) -> Self {
+    static func create(forID id: String, in context:CoreDataContext) -> Self {
         let item = Self(entity: NSEntityDescription.entity(forEntityName: Self.entityName(), in: context.context)!, insertInto: context.context)
         item.setValue(id, forKey: self.idKey())
         
         return item
     }
     
-    public static func create(in context:CoreDataContext) -> Self {
+    static func create(in context:CoreDataContext) -> Self {
         let item = Self(entity: NSEntityDescription.entity(forEntityName: Self.entityName(), in: context.context)!, insertInto: context.context)
         context.add(item)
         return item
     }
     
-    public static func findOrCreate(in context:CoreDataContext, id: String) -> Self {
+    static func findOrCreate(in context:CoreDataContext, id: String) -> Self {
         return self.object(forID: id, in: context) ?? self.create(forID: id, in: context)
     }
     
